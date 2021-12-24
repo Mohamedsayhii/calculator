196148961ca1ea436929f254b42bf6cca216dcad
Mohamedsayhii <mohamedsaihii@outlook.fr>
2021-12-24 20:33:14 +0100

    completed the operations, still verifying some details

--- script.js
+++ script.js
@@ -1,3 +1,107 @@
+let currentOperation;
+let firstOperand;
+let secondOperand;
+
+const currentOperationScreen = document.querySelector("#currentOperation");
+const lastOperationScreen = document.querySelector("#lastOperation");
+
+const clearBtn = document.querySelector("#clearButton");
+const deleteBtn = document.querySelector("#deleteButton");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const pointBtn = document.getElementById("pointBtn");
+
+//display
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    currentOperationScreen.textContent == 0
+      ? (currentOperationScreen.textContent = button.textContent)
+      : (currentOperationScreen.textContent += button.textContent);
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (
+      currentOperationScreen.textContent == "" &&
+      lastOperationScreen.textContent == ""
+    )
+      return;
+    firstOperand = currentOperationScreen.textContent;
+    currentOperationScreen.textContent = "";
+    lastOperationScreen.textContent = firstOperand + " " + button.textContent;
+
+    currentOperation = button.textContent;
+  });
+});
+
+equalsButton.addEventListener("click", compute);
+deleteBtn.addEventListener("click", deleteNumber);
+pointBtn.addEventListener("click", appendPoint);
+clearBtn.addEventListener("click", clear);
+
+function appendPoint() {
+  if (currentOperationScreen.textContent.includes(".")) return;
+  currentOperationScreen.textContent == ""
+    ? (currentOperationScreen.textContent = "0.")
+    : (currentOperationScreen.textContent += ".");
+}
+
+function compute() {
+  if (
+    currentOperationScreen.textContent == "" &&
+    lastOperationScreen.textContent == ""
+  )
+    return;
+  if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
+    alert("You can't divide by 0!");
+    currentOperationScreen.textContent = "";
+    return;
+  }
+
+  lastOperationScreen.textContent +=
+    " " + currentOperationScreen.textContent + " =";
+  secondOperand = currentOperationScreen.textContent;
+  console.log(`second operand is ${secondOperand}`);
+  let result = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  currentOperationScreen.textContent = result;
+}
+
+function clear() {
+  currentOperationScreen.textContent = "";
+  lastOperation.textContent = "";
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1);
+}
+
+function reloadScreen() {}
+
+//keyboard
+// function handleKeyboardInput(e) {
+//   if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+//   if (e.key === ".") appendPoint();
+//   if (e.key === "=" || e.key === "Enter") evaluate();
+//   if (e.key === "Backspace") deleteNumber();
+//   if (e.key === "Escape") clear();
+//   if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+//     operate(convertOperator(e.key));
+// }
+
+// function convertOperator(keyboardOperator) {
+//   if (keyboardOperator === "/") return "÷";
+//   if (keyboardOperator === "*") return "x";
+//   if (keyboardOperator === "-") return "-";
+//   if (keyboardOperator === "+") return "+";
+// }
+
 //operations
 function add(a, b) {
   return a + b;
@@ -19,16 +123,24 @@ function power(a, b) {
   return Math.pow(a, b);
 }
 
-function operate(operation, a, b) {
-  return operation(a, b);
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
 }
 
-//display
-let currentOperation = document.querySelector(".current-operation");
-let buttons = document.querySelectorAll(".numbers button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    currentOperation.textContent = button.textContent;
-  });
-});
+function operate(operation, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operation) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
